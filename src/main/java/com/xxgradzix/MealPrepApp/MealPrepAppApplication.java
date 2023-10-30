dfc5a0561de3c8d726472c21369a73baf5629a1a
Kuba Grad <xxgradzix@gmail.com>
2023-10-30 08:48:49 +0100

    initial commit

---
+++ src/main/java/com/xxgradzix/MealPrepApp/MealPrepAppApplication.java
@@ -0,0 +1,13 @@
+package com.xxgradzix.MealPrepApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPrepAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPrepAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xxgradzix/MealPrepApp/MealPrepAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxgradzix.MealPrepApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPrepAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
